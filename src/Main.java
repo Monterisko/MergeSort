6c5d971e19fb507eeac4dea6291f78b1ad81d7a6
monte <Monte@pc>
2023-11-10 01:10:11 +0100

    Final Project

--- src/Main.java
+++ src/Main.java
@@ -19,44 +19,44 @@ public class Main {
         }
     }
 
-    static void InsertionSort(Integer[] arr)
+    static void InsertionSort(Integer[] array)
     {
-        int n = arr.length;
+        int n = array.length;
         for (int i = 1; i < n; ++i) {
-            int key = arr[i];
+            int key = array[i];
             int j = i - 1;
 
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
                 j = j - 1;
             }
-            arr[j + 1] = key;
+            array[j + 1] = key;
         }
     }
 
-    public static void MergeInsertSort(Integer[] tab, int start, int stop, int InsertStart){
-        if(tab.length > InsertStart){
+    public static void MergeInsertSort(Integer[] array, int start, int stop, int InsertStart){
+        if(array.length > InsertStart){
             if(start < stop){
                 int m = (start + stop) / 2;
-                MergeSort(tab, start, m);
-                MergeSort(tab, m + 1, stop);
-                Merge(tab, start, m, stop);
+                MergeInsertSort(array, start, m, InsertStart);
+                MergeInsertSort(array, m + 1, stop, InsertStart);
+                Merge(array, start, m, stop);
             }
         }
         else {
-            InsertionSort(tab);
+            InsertionSort(array);
         }
     }
-    public static void MergeSort(Integer[] tab, int start, int stop){
+    public static void MergeSort(Integer[] array, int start, int stop){
         if(start < stop){
             int m = (start + stop) / 2;
-            MergeSort(tab, start, m);
-            MergeSort(tab, m + 1, stop);
-            Merge(tab, start, m, stop);
+            MergeSort(array, start, m);
+            MergeSort(array, m + 1, stop);
+            Merge(array, start, m, stop);
         }
     }
 
-    public static void Merge(Integer[] tab, int start, int mid, int stop){
+    public static void Merge(Integer[] array, int start, int mid, int stop){
         int n1 = mid - start + 1;
         int n2 = stop - mid;
 
@@ -64,32 +64,32 @@ public class Main {
         int[] R = new int[n2];
 
         for (int i = 0; i < n1; i++) {
-            L[i] = tab[start + i];
+            L[i] = array[start + i];
         }
         for (int i = 0; i < n2; i++) {
-            R[i] = tab[mid + i + 1];
+            R[i] = array[mid + i + 1];
         }
 
         int i = 0, j = 0;
         int k = start;
         while (i < n1 && j < n2){
             if(L[i] <= R[j]){
-                tab[k] = L[i];
+                array[k] = L[i];
                 i++;
             }
             else {
-                tab[k] = R[j];
+                array[k] = R[j];
                 j++;
             }
             k++;
         }
         while (i < n1){
-            tab[k] = L[i];
+            array[k] = L[i];
             ++i;
             ++k;
         }
         while (j < n2) {
-            tab[k] = R[j];
+            array[k] = R[j];
             j++;
             k++;
         }
